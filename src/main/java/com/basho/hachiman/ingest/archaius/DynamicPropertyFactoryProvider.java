c4fa8b867e432b4961e81b7ab15114b3ad33941d
Jon Brisbin <jbrisbin@basho.com>
2015-11-30 11:15:25 -0600

    Whitespace fix

--- src/main/java/com/basho/hachiman/ingest/archaius/DynamicPropertyFactoryProvider.java
+++ src/main/java/com/basho/hachiman/ingest/archaius/DynamicPropertyFactoryProvider.java
@@ -1,56 +1,50 @@
 package com.basho.hachiman.ingest.archaius;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
+import com.netflix.config.*;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.netflix.config.ConfigurationManager;
-import com.netflix.config.DynamicConfiguration;
-import com.netflix.config.DynamicPropertyFactory;
-import com.netflix.config.FixedDelayPollingScheduler;
-import com.netflix.config.PolledConfigurationSource;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
-public class DynamicPropertyFactoryProvider implements
-        FactoryBean<DynamicPropertyFactory> {
+public class DynamicPropertyFactoryProvider implements FactoryBean<DynamicPropertyFactory> {
 
-    @Value("${hachiman.ingest.checkСonfigRateMs}")
-    private int checkConfigRateMs;
+  @Value("${hachiman.ingest.checkСonfigRateMs}")
+  private int checkConfigRateMs;
 
-    @Autowired
-    private PolledConfigurationSource source;
+  @Autowired
+  private PolledConfigurationSource source;
 
-    private FixedDelayPollingScheduler scheduler;
+  private FixedDelayPollingScheduler scheduler;
 
-    @PostConstruct
-    private void init() {
-        scheduler = new FixedDelayPollingScheduler(0, checkConfigRateMs, false);
-        DynamicConfiguration dynConf = new DynamicConfiguration(source, scheduler);
-        ConfigurationManager.install(dynConf);
-    }
-    
-    @PreDestroy
-    public void shutdown() {
-        scheduler.stop();
-    }
+  @PostConstruct
+  private void init() {
+    scheduler = new FixedDelayPollingScheduler(0, checkConfigRateMs, false);
+    DynamicConfiguration dynConf = new DynamicConfiguration(source, scheduler);
+    ConfigurationManager.install(dynConf);
+  }
 
-    @Override
-    public DynamicPropertyFactory getObject() throws Exception {
-        return DynamicPropertyFactory.getInstance();
-    }
+  @PreDestroy
+  public void shutdown() {
+    scheduler.stop();
+  }
 
-    @Override
-    public Class<?> getObjectType() {
-        return DynamicPropertyFactory.class;
-    }
+  @Override
+  public DynamicPropertyFactory getObject() throws Exception {
+    return DynamicPropertyFactory.getInstance();
+  }
 
-    @Override
-    public boolean isSingleton() {
-        return true;
-    }
+  @Override
+  public Class<?> getObjectType() {
+    return DynamicPropertyFactory.class;
+  }
+
+  @Override
+  public boolean isSingleton() {
+    return true;
+  }
 
 }
