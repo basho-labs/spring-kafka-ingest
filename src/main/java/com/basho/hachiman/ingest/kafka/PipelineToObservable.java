de63cb025d0b5c48dcd40bb01b6ad7d17186e7e1
tmatvienko <taniano4ka@gmail.com>
2015-12-08 12:13:41 +0200

    Removing import of non-existing Pipeline class

--- src/main/java/com/basho/hachiman/ingest/kafka/PipelineToObservable.java
+++ src/main/java/com/basho/hachiman/ingest/kafka/PipelineToObservable.java
@@ -1,23 +1,19 @@
 package com.basho.hachiman.ingest.kafka;
 
 
-
+import com.basho.hachiman.ingest.config.KafkaConfig;
+import com.basho.hachiman.ingest.config.PipelineConfig;
 import kafka.consumer.ConsumerConfig;
-
 import org.springframework.stereotype.Component;
-
 import rx.Observable;
 import rx.functions.Func2;
 import rx.subjects.BehaviorSubject;
 
-import com.basho.hachiman.ingest.Pipeline;
-import com.basho.hachiman.ingest.config.KafkaConfig;
-
 @Component
-public class PipelineToObservable implements Func2<Pipeline, BehaviorSubject<Observable<Record>>, RxKafkaConnector> {
+public class PipelineToObservable implements Func2<PipelineConfig, BehaviorSubject<Observable<Record>>, RxKafkaConnector> {
 
   @Override
-  public RxKafkaConnector call(Pipeline p, BehaviorSubject<Observable<Record>> subject) {
+  public RxKafkaConnector call(PipelineConfig p, BehaviorSubject<Observable<Record>> subject) {
     KafkaConfig kafkaConfig = p.getKafka();
     ConsumerConfig consumerConfig = RxKafkaConnector.createConfig(
         p.getName(), 
