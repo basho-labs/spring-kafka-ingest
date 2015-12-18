d9b1331306a0b9bf38b9e81c9aa86647dd086311
Jon Brisbin <jon@jbrisbin.com>
2015-12-18 10:34:54 -0600

    Changed Python example to use Table instead of client directly.

--- src/test/sh/select_data.py
+++ src/test/sh/select_data.py
@@ -1,7 +1,7 @@
 from riak import RiakClient, RiakNode, Table
 
 cl = RiakClient(host='mesos-4', pb_port=31998)
-keys = cl.ts_stream_keys(Table(cl, 'ingest'))
-for chunk in keys:
-    for key in chunk:
-        print key
+tbl = Table(cl, 'ingest')
+for chunk in tbl.stream_keys():
+  for key in chunk:
+    print key
